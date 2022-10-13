a386f9ddfcc679ff6e89482bcfc4b8b0a29c655e
COOL- <87934102+msajo@users.noreply.github.com>
2022-10-13 16:41:18 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-mouseEvent = ""
+color = "pink";
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
-color = "red";
-width = "11"
-radius = "5";
-canvas.addEventListener("mouseDown", my_mousedown);
-canvas.addEventListener("mouseMove", my_mousemove);
-canvas.addEventListener("mouseUp", my_mouseup);
-canvas.addEventListener("mouseLeave", my_mouseleave);
-
-function my_mousedown (e);
+mouseEvent = "";
+lastX = 0;
+lastY = 0;
+canvas.addEventListener("mouseup", my_mouseup);
+function my_mouseup(e)
 {
-    color = document.getElementById("color").value;
-    width_of_line = document.getElementById("width").value;
-    radius = document.getElementById("radius").value;
-    mouseEvent = "mouseDown";
+    mouseEvent = "mouseUP";
 }
 
+canvas.addEventListener("mouseleave", my_mouseleave);
+function my_mouseleave(e)
+{
+    mouseEvent = "mouseLEAVE";
+}
 
+canvas.addEventListener("mousedown", my_mousedown);
+function my_mousedown(e)
+{
+    mouseEvent = "mouseDOWN";
+}
+
+canvas.addEventListener("mousemove", my_mousemove);
 function my_mousemove(e)
 {
     current_position_of_mouse_X = e.clientX - canvas.offsetLeft;
@@ -27,10 +32,10 @@ function my_mousemove(e)
         ctx.strokestyle = color;
         ctx.lineWidth = 4;
 
-        ctx.moveTo(lastX, lastY);
-        ctx.lineTo(current_position_of_mouse_X, current_position_of_mouse_Y);
+        ctx.arc(current_position_of_mouse_X,current_position_of_mouse_Y,36,0,2*Math.PI);
         ctx.stroke();
     }
     lastX = current_position_of_mouse_X;
     lastY = current_position_of_mouse_Y;
+
 }
